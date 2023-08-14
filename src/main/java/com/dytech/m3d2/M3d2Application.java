391bf6539e7ccd294f27703f7594dedea8d3cba7
Dyeggo Silva <dyeggo0102@live.com>
2023-08-13 23:45:42 -0300

    início

---
+++ src/main/java/com/dytech/m3d2/M3d2Application.java
@@ -0,0 +1,13 @@
+package com.dytech.m3d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3d2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M3d2Application.class, args);
+	}
+
+}
