6ffb598f85d3ba484301cf6e3e681d09073d0309
Dyeggo Silva <dyeggo0102@live.com>
2023-08-14 00:07:37 -0300

    Controller e pág HTML
    foi criado um controller para direcionar a uma pág HTML no  templates

---
+++ src/main/java/com/dytech/m3d2/controllers/homeController.java
@@ -0,0 +1,14 @@
+package com.dytech.m3d2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class homeController {
+	@GetMapping("/")
+	public String index(Model model) {
+		//model.addAttribute("nome","Dyeggo");
+		return "home/index";
+	}
+
+}
